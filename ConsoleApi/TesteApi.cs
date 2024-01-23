4bab9f95bfb27c213bc7148fbc2b31177901394b
Gustavo Henrique <Gustavoproject01@gmail.com>
2024-01-23 12:04:05 -0300

    Commit da API-Fake

---
+++ ConsoleApi/TesteApi.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using QuickType;
+using System;
+using System.Net.Http.Json;
+using static QuickType.User;
+
+namespace ConsoleApi
+{
+    class TesteApi
+    {
+
+        static async Task Main(string[] args)
+        {
+
+                HttpClient client = new HttpClient {BaseAddress = new Uri("https://jsonplaceholder.typicode.com")};
+                var response = await client.GetAsync("todos");    
+                var content = await response.Content.ReadAsStringAsync();            
+                var users = JsonConvert.DeserializeObject<User[]>(content); 
+
+                foreach (var item in users)
+                { 
+                    Console.WriteLine(item.Id);
+
+                    Console.WriteLine(item.Title);
+                }
+
+
+         }
+
+    }
+}
---
+++ ConsoleApi/User.cs
@@ -0,0 +1,14 @@
+﻿namespace QuickType
+{
+    public partial class User
+    {
+        public long UserId { get; set; }
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+  
+    }
+
+}
+
+
